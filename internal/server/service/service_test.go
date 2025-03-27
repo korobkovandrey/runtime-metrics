31be2d077ca93e1e2736f92508a99de9292444a1
korobkovandrey <i@nonic.ru>
2025-03-27 11:39:06 +0300

    iter15: fix

--- internal/server/service/service_test.go
+++ internal/server/service/service_test.go
@@ -28,10 +28,9 @@ func TestService_Find(t *testing.T) {
 		mr, err := model.NewMetricRequest(model.TypeGauge, "test", "1")
 		require.NoError(t, err)
 		want := mr.Clone()
-		ctx := context.TODO()
-		mockRepository.EXPECT().Find(gomock.Eq(ctx), gomock.Eq(mr)).Return(want, nil)
+		mockRepository.EXPECT().Find(gomock.Any(), gomock.Eq(mr)).Return(want, nil)
 		service := NewService(mockRepository)
-		got, err := service.Find(ctx, mr)
+		got, err := service.Find(context.TODO(), mr)
 		assert.NoError(t, err)
 		assert.Same(t, want, got)
 	})
@@ -39,10 +38,9 @@ func TestService_Find(t *testing.T) {
 	t.Run("error", func(t *testing.T) {
 		mr, err := model.NewMetricRequest(model.TypeGauge, "test", "1")
 		require.NoError(t, err)
-		ctx := context.TODO()
-		mockRepository.EXPECT().Find(gomock.Eq(ctx), gomock.Eq(mr)).Return(nil, model.ErrMetricNotFound)
+		mockRepository.EXPECT().Find(gomock.Any(), gomock.Eq(mr)).Return(nil, model.ErrMetricNotFound)
 		service := NewService(mockRepository)
-		got, err := service.Find(ctx, mr)
+		got, err := service.Find(context.TODO(), mr)
 		assert.Nil(t, got)
 		assert.ErrorIs(t, err, model.ErrMetricNotFound)
 	})
@@ -57,20 +55,18 @@ func TestService_FindAll(t *testing.T) {
 			model.NewMetricGauge("test1", 1),
 			model.NewMetricCounter("test2", 1),
 		}
-		ctx := context.TODO()
-		mockRepository.EXPECT().FindAll(gomock.Eq(ctx)).Return(want, nil)
+		mockRepository.EXPECT().FindAll(gomock.Any()).Return(want, nil)
 		service := NewService(mockRepository)
-		got, err := service.FindAll(ctx)
+		got, err := service.FindAll(context.TODO())
 		assert.NoError(t, err)
 		assert.ElementsMatch(t, want, got)
 	})
 
 	t.Run("error", func(t *testing.T) {
-		ctx := context.TODO()
-		mockRepository.EXPECT().FindAll(gomock.Eq(ctx)).
+		mockRepository.EXPECT().FindAll(gomock.Any()).
 			Return([]*model.Metric{}, errors.New("error"))
 		service := NewService(mockRepository)
-		got, err := service.FindAll(ctx)
+		got, err := service.FindAll(context.TODO())
 		assert.Nil(t, got)
 		assert.Error(t, err)
 	})
@@ -85,12 +81,11 @@ func TestService_Update(t *testing.T) {
 		mr, err := model.NewMetricRequest(model.TypeGauge, "test", "1")
 		require.NoError(t, err)
 		want := mr.Clone()
-		ctx := context.TODO()
-		mockRepository.EXPECT().Find(gomock.Eq(ctx), gomock.Eq(mr)).Return(nil, model.ErrMetricNotFound)
-		mockRepository.EXPECT().Create(gomock.Eq(ctx), gomock.Eq(mr)).Return(want, nil)
+		mockRepository.EXPECT().Find(gomock.Any(), gomock.Eq(mr)).Return(nil, model.ErrMetricNotFound)
+		mockRepository.EXPECT().Create(gomock.Any(), gomock.Eq(mr)).Return(want, nil)
 		mockRepository.EXPECT().Update(gomock.Any(), gomock.Any()).MaxTimes(0)
 		service := NewService(mockRepository)
-		got, err := service.Update(ctx, mr)
+		got, err := service.Update(context.TODO(), mr)
 		assert.NoError(t, err)
 		assert.Same(t, want, got)
 	})
@@ -101,12 +96,11 @@ func TestService_Update(t *testing.T) {
 		memMetric := model.NewMetricCounter("test", 1)
 		want := memMetric.Clone()
 		*want.Delta += *mr.Delta
-		ctx := context.TODO()
-		mockRepository.EXPECT().Find(gomock.Eq(ctx), gomock.Eq(mr)).Return(memMetric, nil)
+		mockRepository.EXPECT().Find(gomock.Any(), gomock.Eq(mr)).Return(memMetric, nil)
 		mockRepository.EXPECT().Create(gomock.Any(), gomock.Any()).MaxTimes(0)
-		mockRepository.EXPECT().Update(gomock.Eq(ctx), gomock.Eq(&model.MetricRequest{Metric: want})).Return(want, nil)
+		mockRepository.EXPECT().Update(gomock.Any(), gomock.Eq(&model.MetricRequest{Metric: want})).Return(want, nil)
 		service := NewService(mockRepository)
-		got, err := service.Update(ctx, mr)
+		got, err := service.Update(context.TODO(), mr)
 		assert.NoError(t, err)
 		assert.Same(t, want, got)
 	})
@@ -138,14 +132,13 @@ func TestService_UpdateBatch(t *testing.T) {
 		for _, mr := range mrsReq {
 			want = append(want, mr.Clone())
 		}
-		ctx := context.TODO()
-		mockRepository.EXPECT().FindBatch(gomock.Eq(ctx), gomock.Eq([]*model.MetricRequest{
+		mockRepository.EXPECT().FindBatch(gomock.Any(), gomock.Eq([]*model.MetricRequest{
 			{Metric: model.NewMetricCounter("testNotExist", 3)},
 			{Metric: model.NewMetricCounter("testExist", 13)},
 		})).Return([]*model.Metric{model.NewMetricCounter("testExist", 3)}, nil)
-		mockRepository.EXPECT().CreateOrUpdateBatch(gomock.Eq(ctx), gomock.Eq(mrsReq)).Return(want, nil)
+		mockRepository.EXPECT().CreateOrUpdateBatch(gomock.Any(), gomock.Eq(mrsReq)).Return(want, nil)
 		service := NewService(mockRepository)
-		got, err := service.UpdateBatch(ctx, mrs)
+		got, err := service.UpdateBatch(context.TODO(), mrs)
 		assert.NoError(t, err)
 		assert.Equal(t, want, got)
 	})
